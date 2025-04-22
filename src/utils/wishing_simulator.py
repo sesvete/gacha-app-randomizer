8e0dd6cc8549ba440c0513489fb049202b0ac519
Simon Svetec <simon.svetec@gmail.com>
2025-04-22 23:47:33 +0200

    adds basic pulling functionality

--- src/utils/wishing_simulator.py
+++ src/utils/wishing_simulator.py
@@ -1,7 +1,7 @@
 import random
+from datetime import datetime
 
-pity = 0
-guaranteed = False
+pulls = []
 
 class PulledUnit:
     def __init__(self, date, from_banner, num_of_pulls, unit_name):
@@ -9,6 +9,20 @@ class PulledUnit:
         self.from_banner = from_banner
         self.num_of_pulls = num_of_pulls
         self.unit_name = unit_name
+
+    def __str__(self):
+        """
+        Returns a user-friendly string representation of the PulledUnit.
+        """
+        return f"Pulled '{self.unit_name}' on {self.date} won 50/50 '{self.from_banner}' (after {self.num_of_pulls} pulls)."
+
+    def __repr__(self):
+        """
+        Returns a string representation that can be used to recreate the object.
+        """
+        return (f"PulledUnit(date='{self.date}', from_banner='{self.from_banner}', "
+                f"num_of_pulls={self.num_of_pulls}, unit_name='{self.unit_name}')")
+
 
 
 
@@ -44,19 +58,47 @@ def pull_limited_five_star(guaranteed, is_weapon_banner):
             pull_rate = 0.5
         if win < pull_rate:
             unit_name = "FROM BANNER"
+            from_banner = True
             guaranteed = False
         else:
             unit_name = "STANDARD UNIT"
+            from_banner = False
             guaranteed = True
     else:
         unit_name = "FROM BANNER"
         guaranteed = False
-    return guaranteed, unit_name
+        from_banner = True
+    return guaranteed, unit_name, from_banner
 
 #single pull without pity
-def single_pull():
+def single_pull(num_of_pulls, guaranteed, is_weapon_banner):
+    num_of_pulls += 1
     pull = random.random()
+    if pull < 0.006:
+        guaranteed, unit_name, from_banner = pull_limited_five_star(guaranteed, is_weapon_banner)
+        #tle se mora še izpisat enota
+        pulled_unit = PulledUnit(datetime.now(), guaranteed, num_of_pulls, unit_name)
+        pulls.append(pulled_unit)
+        keep_pulling = False
+    else:
+        keep_pulling = True
+    return num_of_pulls, guaranteed, keep_pulling
 
 #single pull soft pity (32,4%)
+# hočem pullad dokler ne dobim 5 star
+for amount in range(50):
+    num_of_pulls = 0
+    keep_pulling = True
+    while keep_pulling:
+        num_of_pulls, guaranteed, keep_pulling= single_pull(num_of_pulls, False, False)
 
 
+print(pulls)
+
+for unit in pulls:
+    print(unit.num_of_pulls)
+
+
+#TODO: soft pity (from 75 onward the probability should be around 35 %)
+#TODO: guaranteed on 90th pull
+#TODO: implement 50/50 guaranteed win
